12d90ea4641bb9625cc1301adfd5543747a3e8be
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-20 15:03:43 -0700

    fixed on main PC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Team from './components/Team/Team';
 
 function App() {
   return (
+    
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/team' element={<Team/>}/>
+      </Routes>
      
     </div>
   );
---
+++ src/components/Team/Team.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Team() {
+  return (
+    <div className='team_outer_shell'>
+        <h2>Is this thing on?</h2>
+
+    </div>
+  )
+}
+
+export default Team
