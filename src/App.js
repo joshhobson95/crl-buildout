953e23443037847646234e286134358858eeed87
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-21 11:00:12 -0700

    start history page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route,Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Team from './components/Team/Team';
+import History from './components/History/History';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/team' element={<Team/>}/>
+        <Route path='/team' element={<Team />}/>
+        <Route path='/history' element={<History />}/>
       </Routes>
      
     </div>
---
+++ src/components/History/History.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './History.css'
+
+
+function History() {
+  return (
+  <div className='history_outer_container'>
+
+        <div className='history_image_top'>
+        <div className='sunset_image'>
+            <div className='our_story'>
+            <h2>Our Story</h2>
+            </div>
+        </div>
+        </div>
+
+</div>
+  )
+}
+
+export default History
