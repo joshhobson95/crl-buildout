8813b066981e14c3756394ca24614115e7c34304
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-22 14:09:41 -0700

    relationalearning page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route,Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Team from './components/Team/Team';
 import History from './components/History/History';
+import RelationaLearning from './components/RelationaLearning/RelationaLearning';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home />}/>
         <Route path='/team' element={<Team />}/>
         <Route path='/history' element={<History />}/>
+        <Route path='/relationalearning' element={<RelationaLearning/>}/>
       </Routes>
      
     </div>
---
+++ src/components/RelationaLearning/RelationaLearning.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import './RelationaLearning.css'
+
+function RelationaLearning() {
+
+
+  return (
+    <div className='relationalearning_outer_shell'>
+
+<div className='relationalearning_top_background'>
+        <div className='relationalearning_top'>       
+            <h2>RelationaLearning</h2>
+            <div className='relationalearning_top_inner'>
+                <p>
+                Promotes seeing all learning as relational. Focuses on building relationships that support life-long learning. Supports the idea that all learning should be a journey in building relations, establishing connections, and comprehending the interdependencies of our lives. Systematically develops relationships that create a climate of safety, affiliation and belonging. Addresses the development of the whole person - mind, body, spirit. Is personal, practical and playful.
+                </p>
+            </div>
+        </div>
+</div>
+
+        <div className='relationalearning_bullets'>
+                <h3>The Five Critical Learning/Educational Relationships Are:</h3>
+                <ul>
+                    <li>The Leaner Or Leader To Self</li>
+                    <li>The Learner or Leader to Content/Subject Matter</li>
+                    <li>The Learner Or Leader To Instructor/Teacher/Boss</li>
+                    <li>The Learner Or Leader To Peer/Other Learners/Other Leaders</li>
+                    <li>The Learner Or Leader To The Wider Community/Context/Situation</li>
+                </ul>                
+          </div>
+
+
+
+
+    </div>
+  )
+}
+
+export default RelationaLearning
