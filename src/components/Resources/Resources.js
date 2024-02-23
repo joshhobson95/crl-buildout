5d681b8483476417c21116c3aad751d5daaa8d9d
joshhobson95 <joshhobson95@gmail.com>
2024-02-23 14:57:23 -0700

    resources skelly

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Resources.css'
 
 function Resources() {
 
@@ -7,9 +8,8 @@ function Resources() {
   return (
     <div className='resources_outer_shell'>
 
-
+        <h2 className='resources_header'>Resources</h2>
         <div className='resources_top_background'>
-        <h2>Resources</h2>
         <div className='resources_purple_top'>
             <p>Dive into Relational Learning with our curated resources at the Center for Relational Learning. Explore articles, webinars, and more to grow personally and professionally. Start your journey today!</p>
         </div>
@@ -20,20 +20,36 @@ function Resources() {
 
         <div className='resources_mid_page'>
             <div className='resource_block'>
+              <h3>Resource Title</h3>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/02/sunset-scaled.jpg' alt=''/>
+              <p>Resource Description: someting small to describe the content</p>
 
             </div>
             <div className='resource_block'>
+              <h3>Resource Title</h3>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/02/sunset-scaled.jpg' alt=''/>
+              <p>Resource Description: someting small to describe the content</p>
 
             </div>
             <div className='resource_block'>
+              <h3>Resource Title</h3>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/02/sunset-scaled.jpg' alt=''/>
+              <p>Resource Description: someting small to describe the content</p>
 
             </div>
             <div className='resource_block'>
+              <h3>Resource Title</h3>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/02/sunset-scaled.jpg' alt=''/>
+              <p>Resource Description: someting small to describe the content</p>
 
             </div>
             <div className='resource_block'>
+              <h3>Resource Title</h3>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/02/sunset-scaled.jpg' alt=''/>
+              <p>Resource Description: someting small to describe the content</p>
 
             </div>
+        
         </div>
 
 
