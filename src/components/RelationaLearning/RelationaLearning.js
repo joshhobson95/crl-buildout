155f364bc400ed2e9a475c6cecb44d4e1cccf3b2
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-23 16:05:36 -0700

    mobile relationalearning page done

--- src/components/RelationaLearning/RelationaLearning.js
+++ src/components/RelationaLearning/RelationaLearning.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './RelationaLearning.css'
+import circles from '../../assets/circles.gif'
 
 function RelationaLearning() {
 
@@ -19,6 +20,9 @@ function RelationaLearning() {
 </div>
 
         <div className='relationalearning_bullets'>
+          <div className='orange_paint_container'>
+          <img className='orange_paint' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/paintstroke-6-e1708727920302.png' alt=''/>
+          </div>
                 <h3>The Five Critical Learning/Educational Relationships Are:</h3>
                 <ul>
                    <li>Promotes Seeing All Learning As Relational</li>
@@ -26,19 +30,61 @@ function RelationaLearning() {
                    <li> Supports the idea that all learning should be a journey in building relations, establishing connections, and comprehending the interdependencies of our lives</li>
                    <li>Systematically develops relationships that create a climate of safety, affiliation and belonging. </li>
                    <li>Addresses the development of the whole person – mind, body, spirit. Is personal, practical and playful.</li>
-                </ul>                
+                </ul>
+                <div className='orange_paint_container2'>
+          <img className='orange_paint2' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/paintstroke-6-e1708727920302.png' alt=''/>
+          </div>                
           </div>
 
           <div className='relationalearning_mid_page'> 
-          <img />
-          <h2>What is the strategy to make relationshipsthe context for teaching and learning?</h2>
-          <p>Getting people to recognize the wisdom of their own experience is the basis for building their capacity for inter-relating effectively in learning environments of all kinds. The method for building that capacity is rooted in the relational approach, in the context of the learning moment, in restoring a balance of instructing and educating.
-The RelationaLearning™ model outlined below suggests that all learning progresses through four levels of relationship. We are always in a relationship but the quality of that relationship and therefore the quality of learning varies greatly. As we move through each level, we can identify the added power that relationships can bring to our learning.</p>
-
+          <img className='relationalearning_graphic' src={circles} alt=''/>
+          <h2>What is the strategy to make relationships the context for teaching and learning?</h2>
+          <p>Getting people to recognize the wisdom of their own experience is the basis for building their capacity for inter-relating effectively in learning environments of all kinds. The method for building that capacity is rooted in the relational approach, in the context of the learning moment, in restoring a balance of instructing and educating.</p>
+          <p>The RelationaLearning™ model outlined below suggests that all learning progresses through four levels of relationship. We are always in a relationship but the quality of that relationship and therefore the quality of learning varies greatly. As we move through each level, we can identify the added power that relationships can bring to our learning.</p>
           </div>
+
           <div className='relationalearning_quote'>
-            <p>The best thing for disturbances of the spirit is to learn. That is the only thing that never fails. That is the only thing which the poor mind can never exhaust, never alienate, never be tortured by, never fear or distrust, and never dream of regretting. —T.H. White</p>
-            
+            <p>"The best thing for disturbances of the spirit is to learn. That is the only thing that never fails. That is the only thing which the poor mind can never exhaust, never alienate, never be tortured by, never fear or distrust, and never dream of regretting." —T.H. White</p>
+          </div>
+
+          <div className='relationalearning_phases'>
+
+            <h2>Synopsis Of The Four Phases</h2>
+            <div className='phaseone'>
+              <h3>Phase I: Recognition</h3>
+              <p>At the recognition level (the level at which most teaching currently occurs) content is explained through lecture or attained by reading or downloading information from a computer. At this level, the memorization of facts can and often does occur. This information is of little value, however, unless learning is advanced to another level by applying the learning to a project or a problem, refining it through interaction. If not taken to higher relational levels, the learner has simply obtained facts for forgetting.</p>
+
+            </div>
+            <div className='phasetwo'>
+            <h3>Phase II: Understanding</h3>
+            <p>At the understanding level, the facts or concepts are tested through application, interaction and interrelationship. Facts are not simply for short-term memorization. They can now be understood as they relate to other facts and concepts as well as other human beings and the world at large. They are now making sense because they seem to be relevant, useful and of potential value to our lives.</p>
+
+            </div>
+            <div className='phasethree'>
+            <h3>Phase III: Valuing</h3>
+            <p>At the valuing level, we begin to incorporate the concepts with our personal, internal value structures. We have begun to discern the overall value of the learning to us personally and have begun to translate that learning outward. We know that we do not live in isolation and we must integrate this learning with our lives and the lives of others. This is the level at which true human potential for RelationaLearning™ begins to emerge. Both students and teachers begin to have a different experience. Both parties are learning. This level represents the beginning of self-governance by the learner. This is the beginning of creativity and authenticity. This is the level at which the learner begins to decide what really matters.</p>
+
+            </div>
+            <div className='phasefour'>
+            <h3>Phase IV: Co-Creating</h3>
+            <p>This level represents a call to the self, to find one’s own place, way and meaning in relating to the world personally and collectively. This is the highest level of learning and truly equates with RelationaLearning™. Unless learning is pursued to this level, it cannot be fully utilized. At this level, learning is shared with others in meaningful, productive ways that enhance the functioning of the learner, others, and the community. The learner is now part of a larger structure of personal freedom characterized by key elements from each phase: awareness, adaptability, interaction and conscious creation.</p>
+            </div>
+          </div>
+            <div className='purple_paint_container'>
+            <img className='purple_paint' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/paintbrush1.png' alt=''/>
+            </div>
+
+
+          <div className='relationalearning_paper'>
+            <img className='relational_logo' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/CRL-REMASTERED-LOGO-2024-31.png' alt='' />
+            <h3>If you would like to learn more about relationaLearning, click the button below to download Dr George Otero and Susan Chambers-Otero’s Position Paper</h3>
+            <button className='relationalearning_paper_download'>Download</button>
+          </div>
+
+          <div className='relationalearning_bottom_banner'>
+            <h2>Are You Ready To Put Relationships First?</h2>
+              <p>Contact us to exmplore how you can become a better relational leader</p>
+              <button>Contact Us</button>
           </div>
 
 
