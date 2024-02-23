73b79bc1e0f812df4718ea05e0a86c88bba91ce5
joshhobson95 <joshhobson95@gmail.com>
2024-02-23 15:08:57 -0700

    laptop commit

--- src/components/RelationaLearning/RelationaLearning.js
+++ src/components/RelationaLearning/RelationaLearning.js
@@ -12,7 +12,7 @@ function RelationaLearning() {
             <h2>RelationaLearning</h2>
             <div className='relationalearning_top_inner'>
                 <p>
-                Promotes seeing all learning as relational. Focuses on building relationships that support life-long learning. Supports the idea that all learning should be a journey in building relations, establishing connections, and comprehending the interdependencies of our lives. Systematically develops relationships that create a climate of safety, affiliation and belonging. Addresses the development of the whole person - mind, body, spirit. Is personal, practical and playful.
+                In an effort to support creative, productive, desired changes in the experience of those in a learning environment, RelationaLearning™ was created. If a person has the skills to navigate the relational elements of the learning experience, he or she can carry those skills from classroom to classroom, to non-traditional learning environments, to work, and to the larger community.
                 </p>
             </div>
         </div>
@@ -21,15 +21,24 @@ function RelationaLearning() {
         <div className='relationalearning_bullets'>
                 <h3>The Five Critical Learning/Educational Relationships Are:</h3>
                 <ul>
-                    <li>The Leaner Or Leader To Self</li>
-                    <li>The Learner or Leader to Content/Subject Matter</li>
-                    <li>The Learner Or Leader To Instructor/Teacher/Boss</li>
-                    <li>The Learner Or Leader To Peer/Other Learners/Other Leaders</li>
-                    <li>The Learner Or Leader To The Wider Community/Context/Situation</li>
+                   <li>Promotes Seeing All Learning As Relational</li>
+                   <li>Focuses On Building Relationships That Support Life-Long Learnin</li>
+                   <li> Supports the idea that all learning should be a journey in building relations, establishing connections, and comprehending the interdependencies of our lives</li>
+                   <li>Systematically develops relationships that create a climate of safety, affiliation and belonging. </li>
+                   <li>Addresses the development of the whole person – mind, body, spirit. Is personal, practical and playful.</li>
                 </ul>                
           </div>
-          <div>
 
+          <div className='relationalearning_mid_page'> 
+          <img />
+          <h2>What is the strategy to make relationshipsthe context for teaching and learning?</h2>
+          <p>Getting people to recognize the wisdom of their own experience is the basis for building their capacity for inter-relating effectively in learning environments of all kinds. The method for building that capacity is rooted in the relational approach, in the context of the learning moment, in restoring a balance of instructing and educating.
+The RelationaLearning™ model outlined below suggests that all learning progresses through four levels of relationship. We are always in a relationship but the quality of that relationship and therefore the quality of learning varies greatly. As we move through each level, we can identify the added power that relationships can bring to our learning.</p>
+
+          </div>
+          <div className='relationalearning_quote'>
+            <p>The best thing for disturbances of the spirit is to learn. That is the only thing that never fails. That is the only thing which the poor mind can never exhaust, never alienate, never be tortured by, never fear or distrust, and never dream of regretting. —T.H. White</p>
+            
           </div>
 
 
