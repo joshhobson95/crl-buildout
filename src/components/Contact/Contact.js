42dd4e08a1acb78758182d6a0e95ecec253adb92
Josh Hobson <joshuahobson1995@gmail.com>
2024-02-28 17:43:45 -0700

    team css finished

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,13 +5,13 @@ function Contact() {
 
 
   return (
-    <div className='contact_outer_shell'>
-              <div className='contact_logo_container'>
-            <img className='contact_logo' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/yellowwheel.png' alt=''/>
+    <div class='contact_outer_shell'>
+              <div class='contact_logo_container'>
+            <img class='contact_logo' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/yellowwheel.png' alt=''/>
             </div>
 
-        <div className='contact_header'>
-          <div className='contact_header_text'>
+        <div class='contact_header'>
+          <div class='contact_header_text'>
             <h2>Contact Us</h2>
             <p>Fore More Information About our Programs</p>
             <p>Or to hire a CRL consultant for your school or community organization, </p>
@@ -19,35 +19,35 @@ function Contact() {
             <p>The Center for Relationalearning & Relationaleadership</p>
           </div>
    
-            <div className='contact_george_container'>
-                    <img className='contact_george' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/Rectangle-247.png'  alt=''/>
+            <div class='contact_george_container'>
+                    <img class='contact_george' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/Rectangle-247.png'  alt=''/>
             </div>
         </div>
 
 
-<div className='contact_mid_page'>
-<div className='phone_container'>
-    <a className='phone_a_tag' href='tel:5056908123'>
+<div class='contact_mid_page'>
+<div class='phone_container'>
+    <a class='phone_a_tag' href='tel:5056908123'>
         <p> (505) 690-8123</p>
-        <img className='phone_icon' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/phonenemail-1.png' alt=''/>
+        <img class='phone_icon' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/phonenemail-1.png' alt=''/>
     </a>
 </div>
 
-<div className='email_container'>
+<div class='email_container'>
     <a href='mailto:Info@Relationalearning.com'>
         <p>Info@Relationalearning.com</p>
-        <img className='email_icon' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/phonenemail-2.png' alt=''/>
+        <img class='email_icon' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/phonenemail-2.png' alt=''/>
     </a>
 </div>
 </div>
 
-  <div className='email_list'>
-<div className='contact_form_container'>
-  <div className='contact_form_text'>
+  <div class='email_list'>
+<div class='contact_form_container'>
+  <div class='contact_form_text'>
                 <h2>Sign Up For Our Email List</h2>
                 <p>And Stay Up To Date With The Cetner And All Our Offerings</p>
     </div>
-                    <form className='contact_email_form'>
+                    <form class='contact_email_form'>
                          <h2>Join Our Email List!</h2>
                   <div>
                     <label htmlFor="firstName">First Name:</label>
