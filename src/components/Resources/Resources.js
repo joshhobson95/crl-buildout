2d1ca439a68f1ac3f6d9430abcf837cda556ff6a
Josh Hobson <joshuahobson1995@gmail.com>
2024-03-01 16:42:29 -0700

    resources mobiel styling

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -14,29 +14,38 @@ function Resources() {
             <p>Dive into Relational Learning with our curated resources at the Center for Relational Learning. Explore articles, webinars, and more to grow personally and professionally. Start your journey today!</p>
         </div>
         </div>
-
-
-
-
-        <div className='resources_mid_page'>
-        <div className='resource_block1'>
-    <div className='resource_block_inner'>
-        <div className="background-image"></div>
-        <div className='content-div'>
-            <h3>Resource Title</h3>
-            <p>Resource Description: something small to describe the content</p>
+        <div className='resource_green_paint_container'>
+        <img className='resource_green_paint' src='https://dev.relationalearning.com/wp-content/uploads/2024/03/paintbrush1-41.png' alt=''/>
         </div>
-    </div>
-</div>
+
+
+
+
+    <div className='resources_mid_page'>
+          <div className='resource_block1'>
+                <div className='resource_block_inner'>
+                        <h3>Resource Title</h3>
+                        <p>Resource Description: something small to describe the content</p>
+                </div>
+          </div>
+
+
             <div className='resource_block2'>
+            <div className='resource_block_inner'>
               <h3>Resource Title</h3>
               <p>Resource Description: someting small to describe the content</p>
             </div>
+            </div>
+
             <div className='resource_block3'>
+            <div className='resource_block_inner'>
               <h3>Resource Title</h3>
               <p>Resource Description: someting small to describe the content</p>
+            </div>
             </div>
         </div>
+
+        <img className='resource_yellow_paint' src='https://dev.relationalearning.com/wp-content/uploads/2024/02/yellowbigalt.png' alt=''/>
 
 
     </div>
