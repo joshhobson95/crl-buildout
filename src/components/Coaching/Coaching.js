1b06b8b19ca71938a1acb873579738a0a9ae01a0
Josh Hobson <joshuahobson1995@gmail.com>
2024-03-05 15:32:14 -0700

    coaching page- styling close to done

--- src/components/Coaching/Coaching.js
+++ src/components/Coaching/Coaching.js
@@ -28,13 +28,38 @@ function Coaching() {
       </div>
 
       <div className='coaching_icon_banner'>
-        <h3>Learn The Relational Mindset For Any Prupose</h3>
-
-        <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/Book.png' alt='Center for Relational Learning Icon' />
-        <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/handshake.png' alt='Center for Relational Learning Icon' />
-        <img src='https://dev.relationalearning.com/wp-content/uploads/2024/02/A0-11.png' alt='Center for Relational Learning Icon'/>
-
+        <h3>Learn The Relational Mindset For Any Purpose</h3>
+      <div className='coaching_just_icons'>
+        <div>
+              <h4>Teachers</h4>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/Book.png' alt='Center for Relational Learning Icon' />
+        </div>
+        <div>
+              <h4>Professionals</h4>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/handshake.png' alt='Center for Relational Learning Icon' />
+        </div>
+        <div>
+              <h4>Everyday</h4>
+              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/people.png' alt='Center for Relational Learning Icon'/>
+        </div>
       </div>
+      </div>
+
+
+
+
+
+      <div class='coaching_closer'>
+        
+    <div className='coaching_closer_text'>
+    <h3>Relational Coaching</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat.</p>
+    </div>
+    <div className='coaching_image_container'>
+    <img className='coaching_image' src='https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg' alt=''/>
+    </div>
+</div>
+
 
 
 
