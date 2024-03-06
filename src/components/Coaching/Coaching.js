91421142b83402791cb8bc1f52d9d7bcb5a4dd81
Josh Hobson <joshuahobson1995@gmail.com>
2024-03-06 12:45:46 -0700

    coaching done

--- src/components/Coaching/Coaching.js
+++ src/components/Coaching/Coaching.js
@@ -6,7 +6,7 @@ function Coaching() {
 
 
   return (
-    <div className='coaching_outer_shell'>
+    <div class='coaching_outer_shell'>
 
 <div class='coaching_top_background'>
         <div class='coaching_top'>       
@@ -19,17 +19,17 @@ function Coaching() {
         </div>
 </div>
 
-      <div className='coaching_mid_page'>
+      <div class='coaching_mid_page'>
 
 
-      <div className='coaching_opener'>
+      <div class='coaching_opener'>
                 <h2>Benefits To Learning From Us</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat.</p>
       </div>
 
-      <div className='coaching_icon_banner'>
+      <div class='coaching_icon_banner'>
         <h3>Learn The Relational Mindset For Any Purpose</h3>
-      <div className='coaching_just_icons'>
+      <div class='coaching_just_icons'>
         <div>
               <h4>Teachers</h4>
               <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/Book.png' alt='Center for Relational Learning Icon' />
@@ -51,13 +51,24 @@ function Coaching() {
 
       <div class='coaching_closer'>
         
-    <div className='coaching_closer_text'>
+    <div class='coaching_closer_text'>
     <h3>Relational Coaching</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat.</p>
     </div>
-    <div className='coaching_image_container'>
-    <img className='coaching_image' src='https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg' alt=''/>
+    <div class='coaching_image_container'>
+    <img class='coaching_image' src='https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg' alt=''/>
     </div>
+</div>
+
+
+<div class='calendly_container_outer'>
+  <h2>Try Something New This Month</h2>
+  <p>Now booking coaching sessions with George</p>
+  <div class='calendly_container_inner'>
+    
+
+  </div>
+
 </div>
 
 
