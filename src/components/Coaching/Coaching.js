47d893c7a7537d3c4d7ff7e03abb15fcf627c799
Josh Hobson <joshuahobson1995@gmail.com>
2024-03-20 13:20:04 -0600

    team page update

--- src/components/Coaching/Coaching.js
+++ src/components/Coaching/Coaching.js
@@ -6,77 +6,76 @@ function Coaching() {
 
 
   return (
-    <div class='coaching_outer_shell'>
+<div class='coaching_outer_shell'>
 
-<div class='coaching_top_background'>
+    <div class='coaching_top_background'>
         <div class='coaching_top'>       
             <h2>Relational Coaching</h2>
             <div class='coaching_top_inner'>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. 
+                Discover the power of putting relationships first in education and leadership! Our coaching program is your gateway to transformative teaching and leadership. Embrace a paradigm where meaningful connections drive success. Join us and revolutionize your approach to education and leadership today!
                 </p>
             </div>
         </div>
+    </div>
+
+    <div class='coaching_mid_page'>
+
+        <div class='coaching_opener'>
+            <h2>Benefits To Learning With Us</h2>
+    <p>At the helm of the Center for RelationaLearning stands Dr. George Otero, a visionary in the realm of relational coaching. With decades of experience in education and leadership, Dr. Otero is dedicated to revolutionizing traditional approaches by placing relationships at the forefront. Through his innovative coaching methods, he empowers teachers and leaders to cultivate deeper connections within their professional spheres. Dr. Otero's expertise transcends conventional wisdom, offering insights and strategies that go beyond mere instruction, focusing instead on fostering authentic bonds that drive meaningful change.</p>
+
+  <div class='coaching_image_container'>
+                <img class='coaching_image' src='https://dev.relationalearning.com/wp-content/uploads/2024/03/coachingGeorgeEdit.png' alt='Dr. George Otero'/>
+            </div>
+
+ <p>Experience the transformative power of relational coaching with Dr. George Otero. Whether you're a seasoned educator seeking to revitalize your classroom dynamics or a leader aiming to inspire your team, Dr. Otero provides tailored guidance to help you thrive. Sign up for a 30-minute consultation directly through our Calendly feature below. Take the first step towards unlocking your potential as a relational leader and join us in shaping a future where meaningful connections drive success.</p>
+
+
+        </div>
+
+        <div class='coaching_icon_banner'>
+            <h3>Learn The Relational Mindset For Any Purpose</h3>
+            <div class='coaching_just_icons'>
+                <div>
+                    <h4>Teachers</h4>
+                    <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/Book.png' alt='Center for Relational Learning Icon' />
+                </div>
+                <div>
+                    <h4>Professionals</h4>
+                    <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/handshake.png' alt='Center for Relational Learning Icon' />
+                </div>
+                <div>
+                    <h4>Everyday</h4>
+                    <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/people.png' alt='Center for Relational Learning Icon'/>
+                </div>
+            </div>
+        </div>
+
+        <div class='calendly_container_outer'>
+            <div class='calendly_text'>
+                <h2>Try Something new this month</h2>
+                <p>Now Booking Coaching Sessions</p>
+              </div>
+                <div class='calendly_container_inner'>
+                    <div class="calendly-inline-widget" data-url="https://calendly.com/sunmoon-kfy/30min" style="min-width:320px;height:700px;"></div>
+                    <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+                </div>
+        </div>
+
+        <div class='coaching_closer'>
+            <div class='coaching_closer_text'>
+          
+
+
+            </div>
+      
+        </div>
+
+    </div>
+
 </div>
 
-      <div class='coaching_mid_page'>
-
-
-      <div class='coaching_opener'>
-                <h2>Benefits To Learning From Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat.</p>
-      </div>
-
-      <div class='coaching_icon_banner'>
-        <h3>Learn The Relational Mindset For Any Purpose</h3>
-      <div class='coaching_just_icons'>
-        <div>
-              <h4>Teachers</h4>
-              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/Book.png' alt='Center for Relational Learning Icon' />
-        </div>
-        <div>
-              <h4>Professionals</h4>
-              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/handshake.png' alt='Center for Relational Learning Icon' />
-        </div>
-        <div>
-              <h4>Everyday</h4>
-              <img src='https://dev.relationalearning.com/wp-content/uploads/2024/03/people.png' alt='Center for Relational Learning Icon'/>
-        </div>
-      </div>
-      </div>
-
-
-
-
-
-      <div class='coaching_closer'>
-        
-    <div class='coaching_closer_text'>
-    <h3>Relational Coaching</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. Nunc at pharetra purus. Nunc turpis lacus, mollis a sapien sit amet, sollicitudin hendrerit erat.</p>
-    </div>
-    <div class='coaching_image_container'>
-    <img class='coaching_image' src='https://dev.relationalearning.com/wp-content/uploads/2024/03/roadrunner.jpg' alt=''/>
-    </div>
-</div>
-
-
-<div class='calendly_container_outer'>
-  <h2>Try Something New This Month</h2>
-  <p>Now booking coaching sessions with George</p>
-  <div class='calendly_container_inner'>
-    
-
-  </div>
-
-</div>
-
-
-
-
-      </div>
-              
-    </div>
   )
 }
 
