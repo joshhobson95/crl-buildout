4120145def3c013035e04d4ce472c3c83a13b643
Josh Hobson <joshuahobson1995@gmail.com>
2024-03-20 16:52:34 -0600

    video page done

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -7,93 +7,108 @@ function Videos() {
     <div class='videos_outer_shell'>
 
 
-        <div class='videos_top_shell'>
+    <div class='videos_top_shell'>
 
-        <div class='videos_top_background'>
-        <div class='videos_top'>       
-            <h2>Videos</h2>
-            <div class='videos_top_inner'>
-                <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac elit leo. Donec sit amet pharetra nunc, sit amet tincidunt lorem. Morbi vel justo eu leo finibus commodo a at lectus. Mauris non est feugiat, dapibus ipsum ultrices, viverra lorem. 
-                </p>
-            </div>
+    <div class='videos_top_background'>
+    <div class='videos_top'>       
+        <h2> CRL Videos</h2>
+        <div class='videos_top_inner'>
+            <p>
+            Welcome to the Center's Videos page, your gateway to insightful content that brings learning to life. Explore our curated collection of videos designed to inspire, educate, and empower educators and leaders in their journey towards relational excellence.
+            </p>
         </div>
+    </div>
 </div>
 
 
-        <div class='videos_container_main'>
-            <div class='video_container'>
-            <iframe  src="https://www.youtube.com/embed/8iSS71lJKu0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class='video_container_text'>
-                <h3>Benefits to RelationaLearning & RelationaLeadership</h3>
-                <br></br>
-                    <p>An introduction to some of the benefits of a Relational approach. Join Dr. Otero on the beginning of the journey to ensuring all people you have a relationship with feel seen, heard and valued.</p>
-                </div>
-             </div>
-        </div>
-        
-        <div class='videos_container_main'>
-            <div class='video_container'>
-            <iframe  src="https://www.youtube.com/embed/CcvHK4NGngs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class='video_container_text'>
-                <h3>Pathway to Improving your Relationships</h3>
-                <br></br>
-                    <p>Welcome to another installment of our Philosophy, Principles & Practices series!  Join Dr. George as he discusses a pathway with simple steps to help you improve all of the relationships in your life, professional or otherwise!</p>
-                </div>
-             </div>
-        </div>
-
-        <div class='videos_container_main'>
-            <div class='video_container'>
-          <iframe src="https://www.youtube.com/embed/1V6uYom9F_o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class='video_container_text'>
-                <h3>Stages of a Relationship</h3>
-                <br></br>
-                    <p>In this video, Dr. George Otero discusses the different stages/levels to a relationship. We will also get a peek at one of the Center for RelationaLearning & RelationaLeaderships graphics depicting the way to move through these stages to increase your happiness and impact.</p>
-                </div>
-             </div>
-        </div>
-
-        <div class='videos_container_main'>
-            <div class='video_container'>
-            <iframe src="https://www.youtube.com/embed/JYcmiC8to5w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class='video_container_text'>
-                <h3>Relational Intention</h3>
-                <br></br>
-                    <p>Set an intention for every interaction with your colleagues and watch your productivity grow! When you change your focus, you change your life! Let Dr. George give you some insider information on how to foster the best working relationships possible.</p>
-                </div>
-             </div>
-        </div>
-
-        <div class='videos_container_main'>
-            <div class='video_container'>
-           <iframe src="https://www.youtube.com/embed/idqRmAPZqkQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class='video_container_text'>
-                <h3>Relational Intention</h3>
-                <br></br>
-                    <p>Set an intention for every interaction with your colleagues and watch your productivity grow! When you change your focus, you change your life! Let Dr. George give you some insider information on how to foster the best working relationships possible.</p>
-                </div>
-             </div>
-        </div>
-        <div class='videos_container_main'>
-            <div class='video_container'>
-           <iframe src="https://www.youtube.com/embed/idqRmAPZqkQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                <div class='video_container_text'>
-                <h3>Relationships & Tasks</h3>
-                <br></br>
-                    <p>Spend a few minutes with Dr. George Otero discussing how the relationships you foster will impact the tasks you complete.</p>
-                </div>
-             </div>
-        </div>
 
 
 
 
-        </div>
 
-  
+    <div class='videos_container_main'>
+
+<div class="video_opener">
+<h2>In this collection of Videos, we explore the How and Why of RelationaLearning and RelationaLeadership</h2>
+<p>Check back for more CRL videos!</p>
+</div>
+
+
+
+
+
+        <div class='video_container'>
+        <iframe  src="https://www.youtube.com/embed/8iSS71lJKu0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class='video_container_text'>
+            <h3>Benefits to RelationaLearning & RelationaLeadership</h3>
+            <br></br>
+                <p>An introduction to some of the benefits of a Relational approach. Join Dr. Otero on the beginning of the journey to ensuring all people you have a relationship with feel seen, heard and valued.</p>
+            </div>
+         </div>
+    </div>
+    
+    <div class='videos_container_main'>
+        <div class='video_container'>
+        <iframe  src="https://www.youtube.com/embed/CcvHK4NGngs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class='video_container_text'>
+            <h3>Pathway to Improving your Relationships</h3>
+            <br></br>
+                <p>Welcome to another installment of our Philosophy, Principles & Practices series!  Join Dr. George as he discusses a pathway with simple steps to help you improve all of the relationships in your life, professional or otherwise!</p>
+            </div>
+         </div>
+    </div>
+
+    <div class='videos_container_main'>
+        <div class='video_container'>
+      <iframe src="https://www.youtube.com/embed/1V6uYom9F_o" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class='video_container_text'>
+            <h3>Stages of a Relationship</h3>
+            <br></br>
+                <p>In this video, Dr. George Otero discusses the different stages/levels to a relationship. We will also get a peek at one of the Center for RelationaLearning & RelationaLeaderships graphics depicting the way to move through these stages to increase your happiness and impact.</p>
+            </div>
+         </div>
+    </div>
+
+    <div class='videos_container_main'>
+        <div class='video_container'>
+        <iframe src="https://www.youtube.com/embed/JYcmiC8to5w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class='video_container_text'>
+            <h3>Relational Intention</h3>
+            <br></br>
+                <p>Set an intention for every interaction with your colleagues and watch your productivity grow! When you change your focus, you change your life! Let Dr. George give you some insider information on how to foster the best working relationships possible.</p>
+            </div>
+         </div>
+    </div>
+
+    <div class='videos_container_main'>
+        <div class='video_container'>
+       <iframe src="https://www.youtube.com/embed/idqRmAPZqkQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class='video_container_text'>
+            <h3>Relational Intention</h3>
+            <br></br>
+                <p>Set an intention for every interaction with your colleagues and watch your productivity grow! When you change your focus, you change your life! Let Dr. George give you some insider information on how to foster the best working relationships possible.</p>
+            </div>
+         </div>
+    </div>
+    <div class='videos_container_main'>
+        <div class='video_container'>
+       <iframe src="https://www.youtube.com/embed/idqRmAPZqkQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class='video_container_text'>
+            <h3>Relationships & Tasks</h3>
+            <br></br>
+                <p>Spend a few minutes with Dr. George Otero discussing how the relationships you foster will impact the tasks you complete.</p>
+            </div>
+         </div>
+    </div>
+
+
+
 
     </div>
+
+
+
+</div>
   )
 }
 
